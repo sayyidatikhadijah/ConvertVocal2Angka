e9ea4cee251abcf177694ffedc99efaffd3b8bc5
wonder woman <wonder woman@LAPTOP-JRCMS98G>
2018-08-15 09:20:29 +0700

    Tambah tampil per kata

--- src/String/ConvertVocal2Angka.java
+++ src/String/ConvertVocal2Angka.java
@@ -17,6 +17,7 @@ public class ConvertVocal2Angka {
         tampilJudul(identitas);
         String kalimat = tampilInput();
         String convert = vocal2Angka(kalimat);
+        tampilPerKata(kalimat,convert);
     }
         private static String tampilInput()
     {
@@ -39,7 +40,16 @@ public class ConvertVocal2Angka {
             kalimat = kalimat.replace(arConvert[i][0], arConvert[i][1]);
         return kalimat;         
     
-
+        
+    }
+            
+    private static void tampilPerKata(String kalimat, String convert)
+    {
+        String[] arrKal = kalimat.split(" ");
+        String[] arrCon = convert.split(" ");
+        
+        for(int i=0;i<arrKal.length;i++)
+            System.out.println(arrKal[i]+" => "+arrCon[i]);  
     }
 
 }
