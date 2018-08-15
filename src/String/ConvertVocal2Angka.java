f28e53bcf3377b05a131ab1bc380fac1be05ee25
wonder woman <wonder woman@LAPTOP-JRCMS98G>
2018-08-15 09:15:07 +0700

    Tambah Konversi

--- src/String/ConvertVocal2Angka.java
+++ src/String/ConvertVocal2Angka.java
@@ -16,6 +16,7 @@ public class ConvertVocal2Angka {
         String identitas = "Sayyidati Khadijah / X RPL 2 / 32";
         tampilJudul(identitas);
         String kalimat = tampilInput();
+        String convert = vocal2Angka(kalimat);
     }
         private static String tampilInput()
     {
@@ -26,6 +27,19 @@ public class ConvertVocal2Angka {
         System.out.println("Kalimat Asli : "+ kalimat);
         
         return kalimat;
+    }
+            private static String vocal2Angka(String kalimat)
+            {
+    
+        char [][] arConvert =
+        {{'a','4'},{'i','1'},{'u','2'},{'e','3'},{'o','0'}};
+        
+        kalimat = kalimat.toLowerCase();
+        for(int i=0;i<arConvert.length;i++)
+            kalimat = kalimat.replace(arConvert[i][0], arConvert[i][1]);
+        return kalimat;         
+    
+
     }
 
 }
