c1dc7e781624ab11b4a80a68ff47f7e3435cd8aa
wonder woman <wonder woman@LAPTOP-JRCMS98G>
2018-08-15 07:43:37 +0700

    Initial Commit

---
+++ src/String/CobaAplikasiString.java
@@ -0,0 +1,15 @@
+
+package String;
+
+import java.util.Locale;
+
+public class CobaAplikasiString {
+    public static void main(String[] args)
+    {
+      String identitas = "Sayyidati Khadijah / X RPL 2 / 32";
+      System.out.println("Identitas : "+ identitas);
+     
+    }
+}   
+    
+
