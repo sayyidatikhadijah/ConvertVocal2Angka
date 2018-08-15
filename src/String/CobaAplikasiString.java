63b1bee75ca9fd1bde48b2038fabe222bfbc7e17
wonder woman <wonder woman@LAPTOP-JRCMS98G>
2018-08-15 08:23:24 +0700

    Coba method length & IsEmpty

--- src/String/CobaAplikasiString.java
+++ src/String/CobaAplikasiString.java
@@ -9,7 +9,18 @@ public class CobaAplikasiString {
       String identitas = "Sayyidati Khadijah / X RPL 2 / 32";
       System.out.println("Identitas : "+ identitas);
      
-    }
-}   
+    
+    String x = "Operasi";
+        System.out.println("Isi variabel x : "+ x);
+        System.out.println("\""+ x + "\" panjang karakter = "+ x.length());
+        
+        System.out.println("x adalah kosong : " + x.isEmpty());
+        
+        String y = "";
+        System.out.println("isi variabel y : "+ y);
+        System.out.println("y adalah kosong : " + y.isEmpty());
+
+}
+}
     
 
