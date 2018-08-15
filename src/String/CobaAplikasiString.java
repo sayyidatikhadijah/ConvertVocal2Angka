091f779990ba0e8a64bffeeba3e02a8523cd0dff
wonder woman <wonder woman@LAPTOP-JRCMS98G>
2018-08-15 08:27:23 +0700

    Coba method equals & equalsIgnoreCase

--- src/String/CobaAplikasiString.java
+++ src/String/CobaAplikasiString.java
@@ -19,6 +19,20 @@ public class CobaAplikasiString {
         String y = "";
         System.out.println("isi variabel y : "+ y);
         System.out.println("y adalah kosong : " + y.isEmpty());
+        System.out.println("isi x sama dengan y : " + x.equals(y));
+         
+        String z = "Operasi";
+        System.out.println("Isi variabel z : "+ z);
+        System.out.println("Isi x sama dengan z (Case Sensitive): "
+                 + x.equals(z));
+         
+        String r = "operasi";
+        System.out.println("isi variabel r : "+ r);
+        System.out.println("isi variabel r : (Case Sensitive) : " 
+                + x.equals(r));
+        System.out.println("isi variabel r : (Not Case Sensitive) : " 
+                + x.equalsIgnoreCase(r));
+
 
 }
 }
