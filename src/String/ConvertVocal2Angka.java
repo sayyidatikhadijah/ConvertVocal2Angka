49a009abad0d0b52c9b59b480b0e50b7f0a2f758
wonder woman <wonder woman@LAPTOP-JRCMS98G>
2018-08-15 09:21:39 +0700

    Tambah tampil hasil

--- src/String/ConvertVocal2Angka.java
+++ src/String/ConvertVocal2Angka.java
@@ -18,6 +18,7 @@ public class ConvertVocal2Angka {
         String kalimat = tampilInput();
         String convert = vocal2Angka(kalimat);
         tampilPerKata(kalimat,convert);
+        tampilHasil(convert);
     }
         private static String tampilInput()
     {
@@ -50,6 +51,10 @@ public class ConvertVocal2Angka {
         
         for(int i=0;i<arrKal.length;i++)
             System.out.println(arrKal[i]+" => "+arrCon[i]);  
+    }
+    private static void tampilHasil(String convert)
+    {
+        System.out.println("Kalimat Alay Angka : " + convert);
     }
 
 }
