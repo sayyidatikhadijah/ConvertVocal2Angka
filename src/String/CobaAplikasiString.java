d746646d3f6d6d0f9a8db5bb4c6160ed2268dffa
wonder woman <wonder woman@LAPTOP-JRCMS98G>
2018-08-15 08:30:20 +0700

    Coba method contains, toUpperCase, toLowerCase, trim & concat

--- src/String/CobaAplikasiString.java
+++ src/String/CobaAplikasiString.java
@@ -46,6 +46,15 @@ public class CobaAplikasiString {
         System.out.println("perbandingan isi r dengan s (Case Sensitive) : "
                 + r.compareTo(s));
 
+        System.out.println("pada x terdapat huruf 'a' : " + x.contains("a"));
+        System.out.println("isi variabel x besar semua : "+ x.toUpperCase());
+        System.out.println("isi variabel x kecil semua : "+ x.toLowerCase());
+             
+        String t = "   operasi string   ";
+        System.out.println("isi variabel t : \""+ t +"\"");
+        System.out.println("isi variael t tanpa spasi : \""+ t.trim() +"\""); 
+        System.out.println("gabungkan isi variabel x dan r : "+ x.concat(r));
+
 
 }
 }
