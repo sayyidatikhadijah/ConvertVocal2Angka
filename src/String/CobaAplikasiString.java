5d74f47cab989b65559566a326cd9d0ff19096e0
wonder woman <wonder woman@LAPTOP-JRCMS98G>
2018-08-15 08:31:43 +0700

    Coba method substring, replace, & split

--- src/String/CobaAplikasiString.java
+++ src/String/CobaAplikasiString.java
@@ -55,6 +55,24 @@ public class CobaAplikasiString {
         System.out.println("isi variael t tanpa spasi : \""+ t.trim() +"\""); 
         System.out.println("gabungkan isi variabel x dan r : "+ x.concat(r));
 
+        System.out.println("karakter mulai posisi 3 di variabel x : " 
+                + x.substring(3));
+        System.out.println("karakter dari posisi 3-5 di variabel x : "
+                + x.substring(3,6));
+             
+             String tclean = t.trim();
+             System.out.println("isi variabel tclean : "+ t.trim());
+             System.out.println("rubah 'i' jadi 'E' di variabel tolean : "
+                     + tclean.replace("i", "E"));
+             
+             String[] arrT = tclean.split(" ");
+             System.out.println("pecah kaca di variabel tclean : ");
+             for(int i=0;i<arrT.length;i++)
+                 System.out.println(arrT[i]);
+             String[] arrTr = tclean.split("r");
+             System.out.println("pecah berdasarkan huruf 'r' di variabel tclean : ");
+             for(int i=0;i<arrTr.length;i++)
+                 System.out.println(arrTr[i]);
 
 }
 }
